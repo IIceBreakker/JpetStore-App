0be5db2baf8f745f1781d5b29089b34b95066808
IIceBreakker <575987220@qq.com>
2020-05-29 16:22:07 +0800

    add register.

--- app/src/main/java/org/csu/app/LoginActivity.java
+++ app/src/main/java/org/csu/app/LoginActivity.java
@@ -1,14 +1,17 @@
 package org.csu.app;
 
 import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import org.csu.app.domain.Cart;
 import org.csu.app.domain.User;
 import org.csu.app.util.HttpRequest;
 import org.csu.app.util.StreamTool;
@@ -24,6 +27,9 @@ public class LoginActivity extends AppCompatActivity {
     private EditText passwordIn;
     private Button login;
     private String login_url;
+    private String register_url;
+    private String username;
+    private String password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,14 +40,15 @@ public class LoginActivity extends AppCompatActivity {
         login = findViewById(R.id.login);
 
         login_url = URLCollection.LOGIN_URL;
+        register_url = URLCollection.REGISTER_URL;
 
     }
 
     @SuppressLint("StaticFieldLeak")
     public void login(View view) {
 
-        final String username = usernameIn.getText().toString();
-        final String password = passwordIn.getText().toString();
+        username = usernameIn.getText().toString();
+        password = passwordIn.getText().toString();
 
         new AsyncTask<Void, Void, User>() {
 
@@ -52,14 +59,61 @@ public class LoginActivity extends AppCompatActivity {
 
             @Override
             protected void onPostExecute(User user) {
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                AppSession session = (AppSession) getApplication();
-                Cart cart = new Cart();
-                session.setCart(cart);
-                session.setUser(user);
-                startActivity(intent);
+                if (user != null) {
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    AppSession session = (AppSession) getApplication();
+                    session.setUser(user);
+                    startActivity(intent);
+                } else {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                    builder.setTitle("Account Not Exists, Register?")
+                            .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    asynTask2.execute();
+                                }
+
+                            })
+                            .setNegativeButton("No", null);
+                    builder.show();
+                }
             }
         }.execute();
+    }
+
+
+    @SuppressLint("StaticFieldLeak")
+    private final AsyncTask<Void, Void, String> asynTask2 = new AsyncTask<Void, Void, String>() {
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            return httpRegister(username, password);
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            if (result.equals("SUCCESS"))
+                Toast.makeText(LoginActivity.this, "Register Success", Toast.LENGTH_SHORT).show();
+            else
+                Toast.makeText(LoginActivity.this, "Register Failed", Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    private String httpRegister(String u, String p) {
+        String result = "";
+        try {
+            String data = "username=" + u +"&&password=" + p;
+            HttpURLConnection conn = HttpRequest.httpPost(register_url, data);
+            String json = StreamTool.getJsonString(conn);
+            JSONObject jo = new JSONObject(json);
+            result = jo.getString("message");
+
+            return result;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return result;
+        }
     }
 
     private User httpLogin(String u, String p) {
@@ -74,8 +128,7 @@ public class LoginActivity extends AppCompatActivity {
             user.setPassword(jo.getString("password"));
             return user;
         } catch (Exception e) {
-            e.printStackTrace();
+            return null;
         }
-        return user;
     }
 }
--- app/src/main/java/org/csu/app/util/URLCollection.java
+++ app/src/main/java/org/csu/app/util/URLCollection.java
@@ -5,10 +5,10 @@ package org.csu.app.util;
 public class URLCollection {
 
     public static final String LOGIN_URL = "http://10.0.2.2:3000/login";
+    public static final String REGISTER_URL = "http://10.0.2.2:3000/register";
     public static final String CATEGORY_URL = "http://10.0.2.2:3000/category";
     public static final String PRODUCTS_URL = "http://10.0.2.2:3000/products?catId=";
     public static final String ITEMS_URL = "http://10.0.2.2:3000/items?productId=";
-    public static final String ITEM_URL = "http://10.0.2.2:3000/item?itemId=";
     public static final String CART_URL = "http://10.0.2.2:3000/order?userId=";
     public static final String ADD_ORDER_URL = "http://10.0.2.2:3000/addOrder";
 
